5c0e575332df4f220fd05aa70023e88331191838
Andy <aaver1n77898@gmail.com>
2025-09-16 12:39:43 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,45 @@
-from flask import Flask, render_template, request
-from Base import FranchiseRequest, session
-from flask_admin import Admin
+from flask import Flask, render_template, request, flash, redirect, url_for
+from Base import FranchiseRequest, get_db
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
-
+app.secret_key = 'Secret_Key_119'
 #дописать админку
 
+#Узнать почему при смене '/' на '/home' получаю ошибку 404
 @app.route('/', methods = ['GET'])
 def home():
     return render_template('home.html')
 
+#не работает форма? проверь html не id а name
 @app.route('/franchise', methods = ['GET', 'POST'])
-def get_franhcise_info():
+def get_franchise_info():
     if request.method == 'POST':
-        full_name = request.form('name')
-        phone = request.form('phone')
-        email = request.form('email')
-        new_request = FranchiseRequest(full_name = full_name, phone = phone, email = email)
-        session.add(new_request)
-        session.commit()
-        return render_template('franchise.html', success = True)
+        db: Session = get_db()
+        full_name = request.form.get('full_name')
+        phone = request.form.get('phone')
+        email = request.form.get('email')
+        try:
+            new_request = FranchiseRequest(
+                full_name = full_name,
+                phone = phone,
+                email = email
+            )
+            db.add(new_request)
+            db.commit()
+        except Exception as e:
+            db.rollback()
+        finally:
+            db.close
     return render_template('franchise.html')
 
 @app.route('/menu', methods = ['GET'])
 def get_menu():
     return render_template('menu.html')
+
+@app.route('/story', methods = ['GET'])
+def get_philosophy():
+    return render_template('story.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
